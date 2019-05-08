2c4a1e13ccb961f81ab90a841ca7699080d1946a
gaoch <gaospecial@gmail.com>
2019-05-08 10:46:53 +0800

    use consistent color palette for NPs

--- PCR_efficiency_with_NPs.R
+++ PCR_efficiency_with_NPs.R
@@ -8,34 +8,36 @@
 #' # Read data
 #'
 #+ data, message=F
-require(dplyr)
-require(tibble)
-require(readr)
+library(tidyverse)
+library(ggpubr)
+library(ggsci)
+library(ggrepel)
+library(cowplot)
 
 results <- read_csv("data/RT-PCR_results.csv")
 amplification <- read_csv("data/RT-PCR_amplification.csv")
 
-#' # Visualization
-
-#+ load required gg environment, message=F
-require(ggplot2)
-require(ggpubr)
-require(ggsci)
-library(ggrepel)
-
 #+ label of plot
 # NP Labels
-np_labels <- c("Fe2O3" = expression(paste(Fe[2],O[3])),
+np_labels <- c("CK"    = expression(paste(CK,"+")),
+               "Fe2O3" = expression(paste(Fe[2],O[3])),
                "ZnO"   = expression(paste(ZnO)),
                "CeO2"  = expression(paste(Ce,O[2])),
                "Fe3O4" = expression(paste(Fe[3],O[4])),
                "Al2O3" = expression(paste(Al[2],O[3])),
                "CuO"   = expression(paste(CuO)),
-               "TiO2"  = expression(paste(Ti,O[2])),
-               "CK"    = expression(paste(CK,"+")))
+               "TiO2"  = expression(paste(Ti,O[2])))
+# color value
+color_values <- pal_npg("nrc")(length(np_labels))
+names(color_values) <- names(np_labels)
+
+amplification$nps <- factor(amplification$nps, levels = names(np_labels))
+results$nps <- factor(results$nps, levels = names(np_labels))
 # DNA polymerase Labels
 enzyme_labels <- c("extaq" = "Ex Taq","phusion" = "Phusion")
 
+#+ function
+# enzyme labeller
 enzyme_labeller <- function(enzyme){
   return(as.character(enzyme_labels[enzyme]))
 }
@@ -53,72 +55,59 @@ fancy_scientific <- function(l) {
   parse(text=l)
 }
 
+#' # Visualization
+
 #' ## Amplication curve
 #+ plot amplication curve, fig.width=10,fig.asp=0.618
 amplification <- amplification %>%  mutate(label=as.character(np_labels[as.character(nps)]))
 
 
-amplification_style <-  theme_bw() + theme(legend.position = "none")
 
-p1 <- ggplot(filter(amplification,enzyme=="extaq"),aes(Cycle,mean,color=nps,group=nps)) + 
-  geom_line(size=1,show.legend = F) + geom_errorbar(aes(ymin=mean-sd,ymax=mean+sd),show.legend = F) +
-  labs(title = "Ex Taq") + geom_text_repel(mapping = aes(x=42,label=label,color=nps),
-                                           data = filter(amplification,Cycle==40,enzyme=="extaq"),
-                                           direction = "y",xlim=c(40,47),segment.alpha = 0,
-                                           box.padding = 0.1,
-                                           parse = T,show.legend = F,hjust=0) + 
-  xlim(c(NA,47)) +  ylab("Rn") +  scale_y_continuous(labels=fancy_scientific) + 
-  scale_color_npg(labels=np_labels,name="NPs") +
-  amplification_style
+plots <- lapply(c("extaq","phusion"), function(x){
+  ggplot(filter(amplification,enzyme==x),aes(Cycle,mean,color=nps,group=nps)) + 
+    geom_line(size=1,show.legend = F) + 
+    geom_errorbar(aes(ymin=mean-sd,ymax=mean+sd),show.legend = F) +
+    labs(title = enzyme_labeller(x)) + 
+    geom_text_repel(mapping = aes(x=42,label=label,color=nps),
+                    data = filter(amplification,Cycle==40,enzyme==x),
+                    direction = "y",xlim=c(40,47),segment.alpha = 0,
+                    box.padding = 0.1,
+                    parse = T,show.legend = F,hjust=0) + 
+    xlim(c(NA,47)) +  ylab("Rn") +  
+    scale_y_continuous(labels=fancy_scientific) + 
+    scale_color_manual(values = color_values,name="NPs") +
+    theme_bw() + theme(legend.position = "none")
+})
 
-p2 <- ggplot(filter(amplification,enzyme=="phusion"),aes(Cycle,mean,color=nps,group=nps)) + 
-  geom_line(size=1,show.legend = F) + geom_errorbar(aes(ymin=mean-sd,ymax=mean+sd),show.legend = F) +
-  labs(title = "Phusion") + geom_text_repel(mapping = aes(x=42,label=label),
-                                            data = filter(amplification,Cycle==40,enzyme=="phusion"),
-                                            direction = "y",xlim=c(40,47),segment.alpha = 0,
-                                            box.padding = 0.1,
-                                            parse = T,show.legend = F,hjust=0) + 
-  xlim(c(NA,47)) +  ylab("Rn") +  scale_y_continuous(labels=fancy_scientific) + 
-  scale_color_npg(labels=np_labels,name="NPs") +
-  amplification_style
 
-cowplot::plot_grid(p1,p2,labels = "AUTO")
-
+plot_grid(plotlist = plots,labels = "AUTO")
 ggsave("Figure 2.png",path = "plots",dpi=300)
 
 
 #' ### Comparison of Ct values
 #+ fig.width=6,fig.asp=0.618
 
-# plot style
-ct_plot_style <-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
-        axis.line.x = element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position = "none")
+plots <- lapply(c("extaq","phusion"), function(x){
+  data <- results %>% select(enzyme,nps,CT) %>% filter(enzyme==x)
+  ggplot(data,aes(nps,CT,color=nps)) + 
+    geom_boxplot(show.legend = F,outlier.alpha = 0,alpha=I(1/2)) + 
+    geom_jitter(show.legend = F,alpha=1/2,width = 0.25) +
+    stat_compare_means(ref.group = "CK",label = "p.signif",hide.ns = F,method = "t.test") + 
+    #stat_compare_means(label.x = "CK",label.y = 20,method = "anova") +
+    labs(title=enzyme_labeller(x)) +  
+    scale_color_manual(values = color_values,name="NPs") +
+    scale_x_discrete(labels=np_labels) +    
+    ylim(c(12.5,18.5)) +
+    xlab("Nanoparticles") + ylab(expression(italic(C[t]))) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
+          axis.line.x = element_blank(),
+          axis.ticks.x=element_blank(),
+          legend.position = "none")
+  
+})
 
-# left panel
-p1 <- results %>% select(enzyme,nps,CT) %>% filter(enzyme=="extaq") %>% ggplot(aes(nps,CT,color=nps)) + geom_boxplot(show.legend = F,outlier.alpha = 0,alpha=I(1/2)) + 
-  geom_jitter(show.legend = F,alpha=1/2,width = 0.25) +
-  stat_compare_means(ref.group = "CK",label = "p.signif",label.y = 16.5,hide.ns = F,method = "t.test") + 
-  #stat_compare_means(label.x = "CK",label.y = 20,method = "anova") +
-  labs(title="Ex Taq") +  scale_color_npg() +
-  scale_x_discrete(labels=np_labels) +   
-  xlab("Nanoparticles") + ylab(expression(italic(C[t]))) + 
-  ct_plot_style
-
-# right panel
-p2 <- results %>% select(enzyme,nps,CT) %>% filter(enzyme=="phusion") %>% ggplot(aes(nps,CT,color=nps)) + geom_boxplot(show.legend = F,outlier.alpha = 0,alpha=I(1/2)) + 
-  geom_jitter(show.legend = F,alpha=1/2,width = 0.25) +
-  stat_compare_means(ref.group = "CK",label = "p.signif",label.y = 19,hide.ns = F,method = "t.test") + 
-  #stat_compare_means(label.x = "CK",label.y = 20,method = "anova") +  
-  labs(title="Phusion") + scale_color_npg() +
-  scale_x_discrete(labels=np_labels) +   
-  xlab("Nanoparticles") + ylab(expression(italic(C[t]))) + 
-  ct_plot_style
-
-
-cowplot::plot_grid(p1,p2,labels = "AUTO")
+plot_grid(plotlist = plots,labels = "AUTO")
 
 ggsave("Figure 3.png",path = "plots",dpi=300)
 
--- error_rate_from_matrix.R
+++ error_rate_from_matrix.R
@@ -7,10 +7,11 @@
 
 #' ## Import, Transform and Tidy
 #' 
-require(tidyr)
-require(dplyr)
-require(tibble)
-require(readr)
+library(tidyverse)
+library(ggpubr)
+library(ggsci)
+library(ggrepel)
+library(cowplot)
 
 # error rate and snp frequency were estimated using amplicon sequencing and mothur software
 error_rate <- read_csv("data/error_rate.csv")
@@ -18,33 +19,26 @@ snp_freq <- read_csv("data/snp_freq.csv")
 
 # read meta
 meta <- read_tsv("data/meta.txt")
-meta$nps <- factor(meta$nps, levels = c("CK","ZnO","Fe2O3","CeO2","Fe3O4","Al2O3","CuO","TiO2"))
-meta$enzyme <- factor(meta$enzyme,levels = c("extaq","phusion"))
-
-# join data
-error_rate <- left_join(error_rate,meta)
-snp_freq <- left_join(snp_freq,meta)
-
-#' ## Data Visualization
-
-#+ load required gg environment, message=F
-require(ggplot2)
-require(ggpubr)
-require(ggsci)
 
 #+ label of plot
 # NP Labels
-np_labels <- c("Fe2O3" = expression(paste(Fe[2],O[3])),
+np_labels <- c("CK"    = expression(paste(CK,"+")),
+               "Fe2O3" = expression(paste(Fe[2],O[3])),
                "ZnO"   = expression(paste(ZnO)),
                "CeO2"  = expression(paste(Ce,O[2])),
                "Fe3O4" = expression(paste(Fe[3],O[4])),
                "Al2O3" = expression(paste(Al[2],O[3])),
                "CuO"   = expression(paste(CuO)),
-               "TiO2"  = expression(paste(Ti,O[2])),
-               "CK"    = expression(paste(CK,"+")))
-# DNA polymerase Labels
-enzyme_labels <- c("extaq" = "TaKaRa Ex Taq","phusion" = "ThermoFisher Phusion")
+               "TiO2"  = expression(paste(Ti,O[2])))
+# color value
+color_values <- pal_npg("nrc")(length(np_labels))
+names(color_values) <- names(np_labels)
 
+# DNA polymerase Labels
+enzyme_labels <- c("extaq" = "Ex Taq","phusion" = "Phusion")
+
+#+ function
+# enzyme labeller
 enzyme_labeller <- function(enzyme){
   return(as.character(enzyme_labels[enzyme]))
 }
@@ -62,81 +56,64 @@ fancy_scientific <- function(l) {
   parse(text=l)
 }
 
+meta$nps <- factor(meta$nps, levels = names(np_labels))
+
+# join data
+error_rate <- left_join(error_rate,meta)
+snp_freq <- left_join(snp_freq,meta)
+
+#' ## Data Visualization
+
 
 #' Overall Error Rate - comparing with CK
 #+ overall error rate, fig.width=6,fig.asp=0.618
-p1 <- filter(error_rate,enzyme=="extaq") %>% ggplot(aes(nps,error_rate,color=nps)) + 
-  geom_boxplot(show.legend=F,outlier.alpha = 0) + 
-  geom_jitter(show.legend=F) + 
-  stat_compare_means(label = "p.signif",hide.ns = T,ref.group = "CK",show.legend = F,method = "t.test") +
-  scale_x_discrete(labels=np_labels) +   
-  scale_y_continuous(labels=fancy_scientific) +
-  scale_color_npg() +
-  labs(title="Ex Taq") +
-  xlab("Nanoparticles") + ylab("Overall Error Rate") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
-        axis.line.x = element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.title.y = element_text(margin = margin(t=0,r=15,b=0,l=0)),
-        legend.text.align = 0) # 0-left,1-right
-p2 <- filter(error_rate,enzyme=="phusion") %>% ggplot(aes(nps,error_rate,color=nps)) + 
-  geom_boxplot(show.legend=F,outlier.alpha = 0) + 
-  geom_jitter(show.legend=F) + 
-  stat_compare_means(label = "p.signif",hide.ns = T,ref.group = "CK",show.legend = F,method = "t.test") +
-  scale_x_discrete(labels=np_labels) +   
-  scale_y_continuous(labels=fancy_scientific) +
-  scale_color_npg() +
-  labs(title="Phusion") +
-  xlab("Nanoparticles") + ylab("Overall Error Rate") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
-        axis.line.x = element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.title.y = element_text(margin = margin(t=0,r=15,b=0,l=0)),
-        legend.text.align = 0) # 0-left,1-right
-cowplot::plot_grid(p1,p2,labels = "AUTO")
+plots <- lapply(c("extaq","phusion"), function(x){
+  data <- filter(error_rate,enzyme==x)
+  ggplot(data, aes(nps,error_rate,color=nps)) + 
+    geom_boxplot(show.legend = F) +
+    geom_jitter(show.legend=F) + 
+    stat_compare_means(label = "p.signif",hide.ns = T,ref.group = "CK",show.legend = F,method = "t.test") +
+    scale_x_discrete(labels=np_labels) +   
+    scale_y_continuous(labels=fancy_scientific) +
+    scale_color_manual(values = color_values,name="NPs") +
+    labs(title=enzyme_labeller(x)) +
+    xlab("Nanoparticles") + ylab("Overall Error Rate") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
+          axis.line.x = element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.title.y = element_text(margin = margin(t=0,r=15,b=0,l=0)),
+          legend.text.align = 0) # 0-left,1-right
+})
+
+plot_grid(plotlist = plots,labels = "AUTO")
+
 ggsave("Figure 4.png",dpi = 300,path = "plots")
 
 #' SNV Frequency - comparing with CK
 #' For extaq
 #+ extaq snv, fig.width=8,fig.asp=0.5
-p1 <- ggplot(filter(snp_freq,enzyme=="extaq"),aes(nps,freq,color=nps)) + 
-  geom_boxplot(show.legend=F,outlier.alpha = 0) + 
-  geom_jitter(show.legend=F) + 
-  #stat_compare_means(label = "p.signif",hide.ns = T,ref.group = "CK",show.legend = F,method = "t.test") +
-  facet_wrap(~snv,ncol=6)  + 
-  scale_x_discrete(labels=np_labels) +
-  scale_y_continuous(labels=fancy_scientific) +
-  scale_color_npg() +
-  labs(title="Ex Taq") +
-  xlab("Nanoparticles") + ylab("Frequency of SNVs") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
-        axis.line.x = element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.title.y = element_text(margin = margin(t=0,r=15,b=0,l=0)),
-        legend.text.align = 0) # 0-left,1-right
+plots <- lapply(c("extaq","phusion"), function(x){
+  data <- filter(snp_freq,enzyme==x)
+  ggplot(data,aes(nps,freq,color=nps)) + 
+    geom_boxplot(show.legend=F,outlier.alpha = 0) + 
+    geom_jitter(show.legend=F) + 
+    #stat_compare_means(label = "p.signif",hide.ns = T,ref.group = "CK",show.legend = F,method = "t.test") +
+    facet_wrap(~snv,ncol=6)  + 
+    scale_x_discrete(labels=np_labels) +
+    scale_y_continuous(labels=fancy_scientific) +
+    scale_color_manual(values = color_values,name="NPs") +
+    labs(title=enzyme_labeller(x)) +
+    xlab("Nanoparticles") + ylab("Frequency of SNVs") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
+          axis.line.x = element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.title.y = element_text(margin = margin(t=0,r=15,b=0,l=0)),
+          legend.text.align = 0) # 0-left,1-right
+})
 
-#' For Phusion
-#+ phusion snv, fig.width=8,fig.asp=0.5
-p2 <- ggplot(filter(snp_freq,enzyme=="phusion"),aes(nps,freq,color=nps)) + 
-  geom_boxplot(show.legend=F,outlier.alpha = 0) + 
-  geom_jitter(show.legend=F) + 
-  #stat_compare_means(label = "p.signif",hide.ns = T,ref.group = "CK",show.legend = F,method = "t.test") +
-  facet_wrap(~snv,ncol=6)  + 
-  scale_x_discrete(labels=np_labels) +
-  scale_y_continuous(labels=fancy_scientific) +
-  scale_color_npg() +
-  labs(title="Phusion") +
-  xlab("Nanoparticles") + ylab("Frequency of SNVs") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1,face = 'bold.italic'),
-        axis.line.x = element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.title.y = element_text(margin = margin(t=0,r=15,b=0,l=0)),
-        legend.text.align = 0) # 0-left,1-right
 #' Put together
 #+ cowplot, fig.width=8,fig.asp=1
-cowplot::plot_grid(p1,p2,ncol=1,align = "v",labels="AUTO")
+plot_grid(plotlist = plots,ncol=1,align = "v",labels="AUTO")
 ggsave("Figure 5.png",dpi = 300,path = "plots")
